5a0a967f5fca9783a4b843033b817752edc786b3
Kris Redding <kredding@autoscribe.com>
2017-11-02 16:55:08 -0400

    build initial home page

---
+++ app/js/scripts.js
@@ -0,0 +1,3 @@
+window.addEventListener('scroll', function(e) {
+    palletetown.scrollcontrol(10, 'navbar-fixed-top', 'moved-header', true);    
+});
--- gulpfile.js
+++ gulpfile.js
@@ -17,11 +17,29 @@ var config = {
     appJs:['app/js/*.js'],
     appLess: ['app/less/*.less'],
     appImgs: ['app/assets/images/**'],
-    libsJs: [],
-    libsCSS: [],
-    libsFonts: ['app/assets/fonts/**'],
+    libsJs: [
+      'app/assets/libs/jquery/dist/jquery.min.js',
+      'app/assets/libs/bootstrap/dist/js/bootstrap.min.js'
+    ],
+    libsCSS: [
+      'app/assets/libs/bootstrap/dist/css/bootstrap.min.css',      
+      'app/assets/libs/font-awesome/css/font-awesome.min.css',
+      'app/assets/libs/animate.css/animate.min.css'
+    ],
+    libsFonts: ['app/assets/fonts/**', 'app/assets/libs/bootstrap/fonts/**', 'app/assets/libs/font-awesome/fonts/**'],
     htmlFiles: [ {'filename': 'index', 'locationname':'index.html'}],
-    navigationLinks:[['{home}','index.html']],    
+    navigationLinks:[
+      ['{home}','index.html'],
+      ['{our-company}','index.html'],
+      ['{goals}','index.html'],
+      ['{our-leadership}','index.html'],
+      ['{community}','index.html'],
+      ['{careers}','index.html'],
+      ['{services}','index.html'],
+      ['{contract-vehicles}','index.html'],
+      ['{partners}','index.html'],
+      ['{contact-us}','index.html']
+    ],    
     htmlLocation: "app/templates/"
   },
   dest:{
@@ -82,8 +100,7 @@ gulp.task('build-html', function(){
       console.log("Building File: " + fileObj.filename);
       return gulp.src(config.src.htmlLocation + '/layout.html')
                 /* Build each page with layout file */
-                .pipe(gfi({"/* INSERT BODY */": config.src.htmlLocation + fileObj.locationname  }))
-                .pipe(gfi({"/* INSERT LOGO */": 'app/assets/images/logo.svg'  }))
+                .pipe(gfi({"/* INSERT BODY */": config.src.htmlLocation + fileObj.locationname  }))                
                 /* Insert proper location into the page links */
                 .pipe(gbr(config.src.navigationLinks))
                 .pipe(rename(function(path){ path.basename = fileObj.filename; }))
